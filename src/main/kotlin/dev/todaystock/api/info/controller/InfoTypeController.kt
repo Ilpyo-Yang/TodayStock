690436dde25dd23ff560f58c6e6faffee7ae94ab
Ilpyo-Yang <ilpyoyang@gmail.com>
2024-11-15 20:35:59 +0900

    Add: Swagger API 추가

--- src/main/kotlin/dev/todaystock/api/info/controller/InfoTypeController.kt
+++ src/main/kotlin/dev/todaystock/api/info/controller/InfoTypeController.kt
@@ -1,14 +1,13 @@
 package dev.todaystock.api.info.controller
 
-import dev.todaystock.api.collect.dto.CollectRequest
-import dev.todaystock.api.collect.dto.CollectResponse
 import dev.todaystock.api.common.dto.ApiResponse
 import dev.todaystock.api.common.exception.ErrorCode
 import dev.todaystock.api.info.dto.InfoTypeRequest
 import dev.todaystock.api.info.dto.InfoTypeResponse
 import dev.todaystock.api.info.entity.InfoType
 import dev.todaystock.api.info.service.InfoTypeService
-import jakarta.validation.Valid
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -16,25 +15,29 @@ import java.util.*
 
 @RestController
 @RequestMapping("/v1/info/{infoType}")
+@Tag(name = "InfoType API", description = "검색한 정보유형과 관련된 API입니다. Type 종류는 Company, Country, Theme 세 종류입니다.")
 class InfoTypeController(
     private val infoTypeService: InfoTypeService
 ) {
     @GetMapping
+    @Operation(summary = "정보 유형 검색", description = "정보 유형 검색 API")
     fun findByInfoType(@PathVariable infoType: InfoType): ResponseEntity<ApiResponse<List<InfoTypeResponse?>>> {
         return ResponseEntity.ok(ApiResponse.successDataResponse(infoTypeService.findAll(infoType)))
     }
 
     @PostMapping
+    @Operation(summary = "정보 유형 생성", description = "정보 유형 생성 API")
     fun create(@PathVariable infoType: InfoType,
                @RequestBody request: InfoTypeRequest
     ): ResponseEntity<ApiResponse<InfoTypeResponse?>> {
         return ResponseEntity.ok(ApiResponse.successDataResponse(infoTypeService.create(infoType, request)))
     }
 
-    // admin
     @DeleteMapping
+    @Operation(summary = "정보 유형 삭제", description = "정보 유형 삭제 API")
     fun delete(@PathVariable infoType: InfoType,
-               @RequestBody(required = true) uuid: UUID): ResponseEntity<ApiResponse<out HttpStatus>> {
+               @RequestBody(required = true) uuid: UUID
+    ): ResponseEntity<ApiResponse<out HttpStatus>> {
         if(infoTypeService.delete(infoType, uuid)) {
             return ResponseEntity.ok(ApiResponse.successResponse())
         }
--- src/main/kotlin/dev/todaystock/api/search/controller/SearchController.kt
+++ src/main/kotlin/dev/todaystock/api/search/controller/SearchController.kt
@@ -4,6 +4,8 @@ import dev.todaystock.api.chat.service.SearchService
 import dev.todaystock.api.common.dto.ApiResponse
 import dev.todaystock.api.info.entity.InfoType
 import dev.todaystock.api.info.entity.MarkerInfo
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/search")
+@Tag(name = "Search API", description = "정보 검색을 위한 API입니다.")
 class SearchController (
     private val searchService: SearchService
 ) {
     @GetMapping
+    @Operation(summary = "최신 뉴스 검색", description = "최신 뉴스 검색을 바탕으로 AI로 정보를 추가해서 정보를 저장하는 API")
     fun searchInfo(@RequestParam(required = true) keyword: String,
                    @RequestParam(required = true) infoType: InfoType
     ): ResponseEntity<ApiResponse<List<MarkerInfo>>> {
